70bf6e2923f393988ee70ea4c51838112bbe54aa
Kyle <Kyle.fire@gmail.com>
2022-07-05 11:07:06 +0900

    added 'Edit' todo functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Todo } from '../model';
 import TodoList from './TodoList';
 import {
@@ -14,6 +14,9 @@ type Props = {
 };
 
 export default function SingleTodo({ todos, todo, setTodos }: Props) {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
   function handleDone(id: number) {
     setTodos(
       todos.map((todo) =>
@@ -26,16 +29,48 @@ export default function SingleTodo({ todos, todo, setTodos }: Props) {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
+  function handleEdit(e: React.FormEvent, id: number) {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className='flex max-w-full gap-2 px-4 py-2 my-4 border-2 rounded-sm border-neutral-900'>
-      {todo.isDone ? (
+    <form
+      onSubmit={(e) => handleEdit(e, todo.id)}
+      className='flex max-w-full gap-2 px-4 py-2 my-4 border-2 rounded-sm border-neutral-900'
+    >
+      {edit ? (
+        <input
+          ref={inputRef}
+          className='px-2 bg-yellow-100'
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
         <s className='w-full text-neutral-500'>{todo.todo}</s>
       ) : (
         <span className='w-full font-bold'>{todo.todo}</span>
       )}
 
       <div className='flex items-center justify-end gap-4 ml-8'>
-        <span className='hover:text-yellow-500'>
+        <span
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+          className='hover:text-yellow-500'
+        >
           <RiEditFill />
         </span>
         <span
